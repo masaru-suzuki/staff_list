fbb80eaf5cae4c289dba668ad84fba5ebfb89f3e
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2020-01-31 06:42:44 +0900

    create staff_add_check.php

---
+++ staf_add.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>ろくまる農園</title>
+</head>
+
+<body>
+  <div class="container" style="margin: 0 300px;">
+    <div class="form-head">
+      <p>スタッフ追加</p>
+    </div>
+    <form action="staff_add_check.php" method="post">
+      <p>スタッフ名を入力してください</p>
+      <input type="text" name="furigana" style="width: 100px;" id="" />
+      <p>パスワードを入力してください</p>
+      <input type="password" name="pass" style="width: 100px" id="">
+      <p>確認のためパスワードをもう1度入力してください</p>
+      <input type="password" name="pass2" style="width: 100px" id="">
+      <hr>
+      <input type="button" onclick="history.back()" value="back">
+      <input type="submit" value="OK">
+    </form>
+  </div>
+</body>
+
+</html>
